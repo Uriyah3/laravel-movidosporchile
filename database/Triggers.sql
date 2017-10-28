bd29595d3a9b8f0aba5f5bc5d0eb5b690ff5c68c
krlos7441 <carlos.sepulveda.a@usach.cl>
2017-10-28 19:54:45 -0300

    2 trigger

--- database/Triggers.sql
+++
@@ -1,4 +0,0 @@
-
-CREATE TRIGGER voluntario_AI AFTER INSERT ON voluntario
-FOR EACH ROW
-INSERT INTO rnv(rut, disponible) VALUES(new.rut, TRUE)
--- database/factories/BienFactory.php
+++ database/factories/BienFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Bien::class, function (Faker $faker) {
     return [
     	'centro_acopio_id' => random_int(\DB::table('centro_acopios')->min('id'), \DB::table('centro_acopios')->max('id')),
     	'tipo_medida_id' => random_int(\DB::table('tipo_medidas')->min('id'), \DB::table('tipo_medidas')->max('id')),
-    	'tipo' => str_random(30),
+    	'tipo' => str_random(30),   //por que?
     	'cantidad' => random_int(1, 50),//hasta 50 cantidades unitarias.
     	'rut' => App\Usuario::all()->random()->rut,
     ];
