4627d9c39344ff5dc63080fa5b751e6d7e8fbfa8
krlod7441 <carlos.sepulveda.a@usach.cl>
2017-10-28 16:01:01 -0300

    Update DepositoFactory.php

--- database/factories/DepositoFactory.php
+++ database/factories/DepositoFactory.php
@@ -7,6 +7,6 @@ $factory->define(App\Deposito::class, function (Faker $faker) {
         'donacion_id' => random_int(\DB::table('donacions')->min('id'), \DB::table('donacions')->max('id')),
         'nombre' => $faker->street_name(),
         'rut' => App\Usuario::all()->random()->rut,
-        'monto' =>  random_int(1, 90000000000000),//es el indicado para esto?
+        'monto' =>  random_int(1, 2000000000),//es el indicado para esto?
     ];
 });
