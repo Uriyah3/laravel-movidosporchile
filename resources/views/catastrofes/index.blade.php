631f50ddd6c0da18ea1a3b0f4388f4130dee2d2c
vagrant <vagrant@homestead>
2017-11-24 21:49:53 +0000

    import model

--- resources/views/catastrofes/index.blade.php
+++ resources/views/catastrofes/index.blade.php
@@ -5,11 +5,40 @@
 @endsection
 
 @section('content')
-@include('layouts.sidebar');
+@include('layouts.sidebar')
 	<head>
 		<title>Catastrofes</title>
 	</head>
 	<body>
-		<h1>Catastrofes identificadas</h1>
+<h2>Catastrofes identificadas</h2>
+<div class="table-responsive">
+            <table class="table table-striped">
+             <thead>
+             <tr>
+				<th>Tipo catastrofe</th>
+				<th>Comuna</th>
+				<th>Descripcion</th>
+				<th>Fecha catastrofe</th>
+			</tr>
+			</thead>	
+			<tbody>
+		@foreach($catastrofes as $catastrofe)
+		<p>
+			<tr>
+                  <td>{{ $catastrofe.tipo_catastrofe.nombre }}</td>
+                  <td>{{$catastrofe.locacion.comuna.nombre}}</td>
+                  <td>{{$catastrofe.descripcion}}</td>
+                  <td>{{$catastrofe.fecha_catastrofe}}</td>
+                  
+            </tr>
+
+
+		</p>
+
+		@endforeach
+
+
+		<!--<h1>Catastrofes identificadas</h1>
+		-->
 	</body>
 @endsection
