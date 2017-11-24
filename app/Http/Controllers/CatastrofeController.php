9768e6c421e97851e543355f8e83079297b68ba2
krlos7441 <carlos.sepulveda.a@usach.cl>
2017-11-24 17:33:43 -0300

    CatastrofeController

--- app/Http/Controllers/CatastrofeController.php
+++ app/Http/Controllers/CatastrofeController.php
@@ -7,32 +7,54 @@ use Illuminate\Http\Request;
 class CatastrofeController extends Controller
 {
 
+    //mostrar todas las catastrofes
     public function index()
     {
-    	return view('catastrofes.index');
+        $catastrofes = Catastrofe::all();
+
+    	return view('catastrofes.index',compact('catastrofes'));
     }
 
 
+    //almacenar nueva catastrofe
     public function store()
     {
+        
+
+        $catastrofe = Catastrofe::create(
+            request(['usuario_id','tipo_catastrofe_id','locacion_id','descripcion','fecha_catastrofe']));
+
+        if($catastrofe-> save()){
+            return view('catastrofes.index');
+        }
+
     	return view('catastrofes.store');
     }
 
 
+    //muestra la vista para crear catastrofe
     public function create()
     {
     	return view('catastrofes.create');
     }
 
 
+    //mostrar una catastrofe
     public function show($id)
     {
-    	return view('catastrofes.show');
+        $catastrofe = Catastrofe::find($id);
+
+    	return view('catastrofes.show',compact('catastrofe'));
     }
 
 
+    //eliminar una catastrofe
     public function delete($id)
     {
+        Catastrofe::destroy($id);
+
+        $request->session()->flash('status','Catastrofe eliminada.');
+
     	return view('catastrofes.delete');
     }
 
