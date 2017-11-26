126dd0b1f0047bf1af6a969bf9adbc835de129f8
Uriyah3 <zerozsaberster2@gmail.com>
2017-11-26 10:41:00 -0300

    Modify login controller instead of a file in vendor folder

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,4 +43,27 @@ class LoginController extends Controller
         Auth::logout();
         return redirect('/');
     }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'rut';
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        $request[$this->username()] = preg_replace('/[\.\-]/i', '',$request[$this->username()]);
+        $request[$this->username()] = substr($request[$this->username()], 0, strlen($request[$this->username()]) - 1);
+        return $request->only($this->username(), 'password');
+    }
 }
