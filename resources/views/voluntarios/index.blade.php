3ae0cd8b493ced96e2e5c92892a7b27380fe48da
vagrant <vagrant@homestead>
2017-11-27 23:06:29 +0000

    controlador voluntario

--- resources/views/voluntarios/index.blade.php
+++ resources/views/voluntarios/index.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.master')
 
+@section('style')
+{{ Html::style('css/perfil.css') }}
+@section('style')
+
+@section('sidebar')
+@include('layouts.sidebarUsuario')
+@endsection
+
 @section('content')
-	<h1>Voluntarios</h1>
+	
+
+<h2 class="subtitulo">Voluntarios</h2>
+
+
+
+<div class="table-responsive"> 
+
+  <table class="table table-striped">
+  	<thead>
+   		<tr>
+				<th>Usuario</th>
+				<th>Voluntariado</th>
+				<th>Fecha inicio</th>
+				<th>¿Finalizado?</th>
+		</tr>
+		</thead>
+		<tbody>
+			@foreach($voluntarios as $voluntario)
+			
+				<tr>
+					<td>{{ $voluntario->usuario->nombre }}</td>
+					<td>{{$voluntario->voluntariado->actividad_voluntariado->nombre}}</td>
+					<td>{{$voluntario->created_at}}</td>
+					<td>{{$voluntario->finalizado==true ? "Verdadero" : "Falso"}}</td>
+				</tr>
+			
+			@endforeach
+
+		</tbody>
+	</table>
+</div>
+{{$voluntarios->links()}}
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -79,7 +79,7 @@ Route::get('/actividades', 'RegistroActividadController@index');
 
 
 Route::post('/voluntarios', 'VoluntarioController@store');
-Route::get('/voluntarios', 'VoluntarioController@indexLog');
+Route::get('/voluntarios', 'VoluntarioController@index');
 Route::get('/voluntarios/{id}', 'VoluntarioController@edit');
 Route::get('/voluntarios/{id}', 'VoluntarioController@show');
 Route::patch('/voluntarios/{id}', 'VoluntarioController@update');
