6d2748e354044524c84036a436c0accdf92ff787
Uriyah3 <zerozsaberster2@gmail.com>
2017-11-28 00:40:53 -0300

    Fix rebase mistake

--- resources/views/comentarios/create.blade.php
+++ resources/views/comentarios/create.blade.php
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-
-	<form action="/comentarios" method="POST">
-	{{ csrf_field() }}
-
-	<h2 class="subtitulo"> Publicar Comentario </h2>
-	<br />
-	<a target="_blank" href="http://movidosporchile.dev/actividades"> {{$user->nombre}} </a>
-	<br />
-	<!--
- 	<h3> Usuario: {{$user->nombre}}</h3>
-	<br />-->
-
-	<div class="form-group">
-		<textarea class="form-control" id="descripcion" name="descripcion" rows="3" placeholder="¿Qué opinas de la medida?"></textarea>
-	</div>
-
-
-	<div class="form-group">
-		<button class="boton" type="submit" class="btn btn-primary">Comentar</button>
-	</div>
-</form>
-=======
 @section('style2')
 {{ Html::style('css/comentario.css') }}
 @endsection
